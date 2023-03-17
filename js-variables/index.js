51a35572b24da060aad8c9c5968c5a8c87348694
zcarian <mar.jed.00@gmail.com>
2023-03-17 10:56:03 +0100

    adding js notes

---
+++ js-variables/index.js
@@ -0,0 +1,40 @@
+var oldUse = "Try not to use";
+
+let newVariableChange = "I can change";
+
+const myValueNotChange = "I cant change!!";
+
+// console.log("oldUse: ", oldUse);
+// console.log("newVariableChange: ", newVariableChange);
+// console.log("myValueNotChange: ", myValueNotChange);
+
+let myBoolean = false;
+
+let myString = "This is a text 1234";
+
+let myBigNumber = 3000n;
+
+let myNull = null;
+
+let myArray = ["Element 1", 2, "3", false, 5];
+
+let myObject = {
+  cohortName: "Savory",
+  amountStudents: 14,
+  academy: "spiced",
+  knowJs: true,
+};
+
+let undefined;
+
+const isANumber = "1";
+let amIaNumber = 1;
+
+let sum = parseInt(isANumber) + amIaNumber;
+console.log(sum);
+
+letMyNewString = myString + " " + amIaNumber + "6";
+
+console.log(letMyNewString, typeof letMyNewString);
+console.log(amIaNumber++);
+console.log((amIaNumber += 1));
