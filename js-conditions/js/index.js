994c5709410ab99b629d1888ff5ccc4cf1014dd9
zcarian <mar.jed.00@gmail.com>
2023-03-20 11:48:59 +0100

    adding js-conditons notes

---
+++ js-conditions/js/index.js
@@ -0,0 +1,31 @@
+// let value;
+
+// value = null;
+
+// if (value) {
+//   console.log("value is Truthy. \t Value: ", value);
+// } else {
+//   console.log("value is Falsy. \t Value: ", value);
+// }
+
+// console.log(typeof value);
+// console.log(typeof typeof value);
+
+// const max = 10;
+// let number = Math.random() * max;
+// number = Math.floor(number);
+
+// let number = Math.floot(Math.random() * max)
+// console.log("Before if Number is", number);
+
+// // boolValue = number > 2;
+
+// number > 2 ? number-- : number++;   //!!!!
+
+// if (number > 2) {
+//   number--;
+// } else {
+//   number++;
+// }
+
+// console.log("After if Number is", number);
