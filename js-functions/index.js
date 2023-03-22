fbb1c433fba0440996735fe0a996430a9d36d261
zcarian <mar.jed.00@gmail.com>
2023-03-22 10:17:46 +0100

    add js-functions notes

--- js-functions/index.js
+++ js-functions/index.js
@@ -1,15 +1,31 @@
-let start = 0;
+// let start = 0;
 
-function Counting(number) {
-  for (let i = 0; i < number; i++) {
-    console.log(i + 1);
-  }
-  start++;
-}
+// function Counting(number) {
+//   for (let i = 0; i < number; i++) {
+//     console.log(i + 1);
+//   }
+//   start++;
+// }
 
-Counting(2);
-Counting(4);
-Counting(3);
-Counting(1);
-console.log("");
-console.log(start);
+// function checkEven(num) {
+//   if (num % 2) {
+//     return "odd";
+//   }
+//   return "even";
+// }
+
+// console.log(checkEven(2));
+// console.log(checkEven(3));
+
+//function declaration
+// function check(num){
+//   return true
+// }
+
+//function expression
+// const check = function(num){
+//   return true
+// }
+
+//Arrow function
+const check = (num) => true;
